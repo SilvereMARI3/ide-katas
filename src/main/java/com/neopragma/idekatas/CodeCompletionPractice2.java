2453cbbfdd3b1b37d86eb56356dcb0952da84f73
Silvere Marie <silvere.marie.ext@protectline.fr>
2023-03-31 09:51:14 +0200

    add missing file

---
+++ src/main/java/com/neopragma/idekatas/CodeCompletionPractice2.java
@@ -0,0 +1,79 @@
+package com.neopragma.idekatas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//    **Key sequences to practice**
+//
+//- (Ctrl + Space) - basic code completion
+//- (Ctrl + Shift + Enter) - complete statement
+//- (Ctrl + Alt + Z) - revert using git
+//- (Delete) - delete selected text
+
+//    public void method1() {
+//        List<Integer> integers = new ArrayList<Integer>();
+//        integers.add(5);
+//        if (integers.isEmpty()) {
+//            throw new RuntimeException("WTF??");
+//        }
+//    }
+
+
+
+public class CodeCompletionPractice2 {
+
+    public void method1() {
+
+    }
+
+
+//Lis
+//(Enter)
+//<Int
+//(Right Arrow)
+//(Space)
+//(Enter)
+//(Ctrl + Shift + Space)
+//(Enter)
+//(Ctrl + Shift + Enter)
+//(Enter)
+//i
+//(Enter)
+//.
+//(Enter)
+//5
+//(Ctrl + Shift + Enter)
+//(Enter)
+//if (i
+//(Enter)
+//.is
+//(Enter)
+//{
+//(Space)
+//(Enter)
+//thro
+//(Enter)
+//n
+//(Enter)
+//R
+//(Enter)
+//"WTF??"
+//(Ctrl + Shift + Enter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
